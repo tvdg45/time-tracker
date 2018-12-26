0189c021d6d526498a5f6f63a9e76cb78ef030cf
Tim van der Graaff <timvdg45@gmail.com>
2018-12-25 21:22:31 -0600

    Create control.java

---
+++ src/main/webapp/controllers/control.java
@@ -0,0 +1,13 @@
+package controllers;
+
+public class Control_Data {
+    
+    public String get_data() {
+    
+      String getting_data = "";
+      
+      getting_data = "hello there";
+      
+      return getting_data;
+    }
+}
