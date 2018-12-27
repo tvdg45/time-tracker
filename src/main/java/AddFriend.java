af6de089446166ec6a51121d5c185372858867f3
Tim van der Graaff <timvdg45@gmail.com>
2018-12-27 14:21:34 -0600

    Update AddFriend.java

--- src/main/java/AddFriend.java
+++ src/main/java/AddFriend.java
@@ -22,19 +22,31 @@ public class AddFriend extends HttpServlet {
 		
 	basic_data_handling.Request_Data declare_request_data = new basic_data_handling.Request_Data();
 		
-	out.println("<html>\n");
-   	out.println("<body>\n");
-      	out.println("<form action = \"https://java-test-9.herokuapp.com/addfriend\" method = \"POST\">\n");
-        out.println("First Name: <input type = \"text\" name = \"first_name\">\n");
-        out.println("<br />\n");
-        out.println("Last Name: <input type = \"text\" name = \"last_name\" />\n");
-        out.println("<input type = \"submit\" value = \"Submit\" />\n");
-      	out.println("</form><br />\n");
+	out.println("<html>");
+   	out.println("<body>");
+      	out.println("<form action = \"https://java-test-9.herokuapp.com/addfriend\" method = \"POST\">");
+        out.println("First Name: <input type = \"text\" name = \"first_name\">");
+        out.println("<br />");
+        out.println("Last Name: <input type = \"text\" name = \"last_name\" />");
+        out.println("<input type = \"submit\" value = \"Submit\" />");
+      	out.println("</form><br />");
 	out.println(request.getParameter("first_name") + "\n\n<br />\n\n" + request.getParameter("last_name"));
-      	out.println("</body>\n");
-	out.println("</html>\n");
+      	out.println("</body>");
+	out.println("</html>");
 		
 	out.println(declare_request_data.get_data());
+	
+	out.println("<br />");
+		
+Class.forName("com.mysql.jdbc.Driver");  
+Connection con=DriverManager.getConnection("jdbc:mysql://localhost/third_party_account_info_per_traffic_monitor_app?user=timothys_tim&password=ranger12");  
+//here sonoo is database name, root is username and password  
+Statement stmt=con.createStatement();  
+ResultSet rs=stmt.executeQuery("select * from emp");  
+while(rs.next()) { 
+out.println(rs.getString(2));  
+con.close();
+}
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
