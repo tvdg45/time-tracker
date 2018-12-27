8fcafc2b13609a8efbe3e46a5ee8c645d8d20265
Tim van der Graaff <timvdg45@gmail.com>
2018-12-27 16:37:03 -0600

    Update AddFriend.java

--- src/main/java/AddFriend.java
+++ src/main/java/AddFriend.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 import java.util.Map;
+
+import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
