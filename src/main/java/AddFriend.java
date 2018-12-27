3f5886c8a3f235f72d2d980e68bfacaa0d9b936f
Tim van der Graaff <timvdg45@gmail.com>
2018-12-27 15:01:52 -0600

    Update AddFriend.java

--- src/main/java/AddFriend.java
+++ src/main/java/AddFriend.java
@@ -47,9 +47,9 @@ Class.forName("com.mysql.jdbc.Driver");
 Connection con=DriverManager.getConnection("jdbc:mysql://sv9.byethost9.org:2083/timothys_digital_solutions_third_party_apps", "timothys_tim", "ranger12");  
 //here sonoo is database name, root is username and password  
 Statement stmt=con.createStatement();  
-ResultSet rs=stmt.executeQuery("select * from third_party_account_info_per_traffic_monitor_app");  
+ResultSet rs=stmt.executeQuery("SELECT first_name FROM third_party_account_info_per_traffic_monitor_app ORDER BY row_id DESC");  
 while(rs.next()) { 
-out.println(rs.getString(2) + "<br />");  
+out.println(rs.getString(0) + "<br />");  
 con.close();
 }
 } catch (Exception e) {
