d50dc78173de995f4fb667b3f3ede0821ba97f92
Tim van der Graaff <timvdg45@gmail.com>
2018-12-27 07:15:38 -0600

    Create Request_Data.java

---
+++ src/main/java/controllers/Request_Data.java
@@ -0,0 +1,13 @@
+package basic_data_handling;
+
+public class Request_Data {
+
+	public String get_data() {
+		
+    String output = "";
+    
+    output += "<h1><b>Welcome, user!</b></h1>\n";
+    
+    return output;
+	}
+}
