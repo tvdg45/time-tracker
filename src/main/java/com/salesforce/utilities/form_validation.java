bfe229df55d67342ed39e8d2dedd255ec9b316e2
Tim van der Graaff <timvdg45@gmail.com>
2019-01-01 23:09:18 -0600

    Update form_validation.java

--- src/main/java/com/salesforce/utilities/form_validation.java
+++ src/main/java/com/salesforce/utilities/form_validation.java
@@ -5,7 +5,11 @@ import java.util.List;
 import java.util.Map;
 
 import java.util.regex.Matcher; 
-import java.util.regex.Pattern; 
+import java.util.regex.Pattern;
+
+import java.util.logging.Level; 
+import java.util.logging.Logger; 
+import java.util.logging.*;  
 
 package form_validation;
 
