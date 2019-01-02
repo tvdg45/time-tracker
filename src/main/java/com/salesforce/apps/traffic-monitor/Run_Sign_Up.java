8a0a02e16c731e16264314a380ed093ffa0aa9bd
Tim van der Graaff <timvdg45@gmail.com>
2019-01-02 00:54:48 -0600

    Update Run_Sign_Up.java

--- src/main/java/com/salesforce/apps/traffic-monitor/Run_Sign_Up.java
+++ src/main/java/com/salesforce/apps/traffic-monitor/Run_Sign_Up.java
@@ -26,11 +26,13 @@ public class Run_Sign_Up extends HttpServlet {
 		String date_received = request.getParameter("date_received");
 		String time_received = request.getParameter("time_received");
 		String sign_up = request.getParameter("sign_up");
-		out.println(sign_up);
+		
 		response.addHeader("Access-Control-Allow-Origin", "https://www.timothysdigitalsolutions.com");
 		response.setContentType("text/html;charset=UTF-8");
 		
 		PrintWriter out = response.getWriter();
+		
+		out.println(sign_up);
 		
 		Control_Sign_Up run_sign_up = new Control_Sign_Up();
 		
