c41d3411778a94cc01b7d1be8f1335c2eb27873a
Tim van der Graaff <timvdg45@gmail.com>
2019-01-01 23:28:40 -0600

    Update and rename control_sign_up.java to Control_Sign_Up.java

--- src/main/java/com/salesforce/controllers/Control_Sign_Up.java
+++ src/main/java/com/salesforce/controllers/Control_Sign_Up.java
@@ -1,11 +1,9 @@
 //Author: Timothy van der Graaff
-package sign_up;
-
-public class Control_Sign_Up extends sign_up.Sign_Up {
+public class Control_Sign_Up extends Sign_Up {
 	
 	public String control_sign_up(first_name, last_name, email, username, password, confirm_password, date_received, time_received, sign_up) {
 		
-		sign_up.Show_Sign_Up_Feedback control_sign_up = new sign_up.Show_Sign_Up_Feedback();
+		Show_Sign_Up_Feedback control_sign_up = new Show_Sign_Up_Feedback();
 		
 		this.set_first_name(first_name);
 		this.set_last_name(last_name);
