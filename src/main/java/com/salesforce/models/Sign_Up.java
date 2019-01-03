8650e4edab488c324b3e3e42ae28fd60defcf0a1
Tim van der Graaff <timvdg45@gmail.com>
2019-01-03 12:26:41 -0600

    Update Sign_Up.java

--- src/main/java/com/salesforce/models/Sign_Up.java
+++ src/main/java/com/salesforce/models/Sign_Up.java
@@ -238,7 +238,7 @@ public abstract class Sign_Up extends Config {
 		String get_time_received = this.get_time_received();
 		String get_sign_up = this.get_sign_up();
 		
-		if (get_sign_up.equals("Sign up") && get_sign_up) {
+		if (get_sign_up.equals("Sign up") && !(get_sign_up.equals(null))) {
 			
 			redundancy_found_in_email = this.repeat_found_in_email(get_email);
 			redundancy_found_in_username = this.repeat_found_in_username(get_username);
