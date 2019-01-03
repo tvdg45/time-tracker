c2c9b02083c1791fae49aec3dec96f786a200f57
Tim van der Graaff <timvdg45@gmail.com>
2019-01-03 12:03:49 -0600

    Update Run_Sign_Up.java

--- src/main/java/com/salesforce/apps/traffic-monitor/Run_Sign_Up.java
+++ src/main/java/com/salesforce/apps/traffic-monitor/Run_Sign_Up.java
@@ -50,7 +50,7 @@ public class Run_Sign_Up extends HttpServlet {
 		out.println("<input type=\"text\" name=\"time_received\" /><br />");
 		out.println("<input type=\"submit\" name=\"sign_up\" value=\"Sign up\" /><br />");
 		out.println("</form>");
-		out.println(run_sign_up.control_sign_up(first_name, last_name, email, username, password, confirm_password, date_received, time_received, sign_up));
+		//out.println(run_sign_up.control_sign_up(first_name, last_name, email, username, password, confirm_password, date_received, time_received, sign_up));
 		out.println("</body>");
 		out.println("</html>");
     }
