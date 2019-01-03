257af72a681e35929113f399da675691219ff903
Tim van der Graaff <timvdg45@gmail.com>
2019-01-02 23:02:23 -0600

    Update Run_Sign_Up.java

--- src/main/java/com/salesforce/apps/traffic-monitor/Run_Sign_Up.java
+++ src/main/java/com/salesforce/apps/traffic-monitor/Run_Sign_Up.java
@@ -30,6 +30,7 @@ public class Run_Sign_Up extends HttpServlet {
 		response.addHeader("Access-Control-Allow-Origin", "https://www.timothysdigitalsolutions.com");
 		response.setHeader("Access-Control-Allow-Methods", "POST");
 		response.setHeader("Access-Control-Allow-Methods", "GET");
+		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
 		response.setContentType("text/html;charset=UTF-8");
 		
 		PrintWriter out = response.getWriter();
