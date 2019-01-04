1ab13c541ecf4c44d89eaac330cab39440e88104
Tim van der Graaff <timvdg45@gmail.com>
2019-01-03 21:33:28 -0600

    Update Run_Sign_Up.java

--- src/main/java/com/salesforce/apps/traffic-monitor/Run_Sign_Up.java
+++ src/main/java/com/salesforce/apps/traffic-monitor/Run_Sign_Up.java
@@ -53,6 +53,61 @@ public class Run_Sign_Up extends HttpServlet {
 		out.println(run_sign_up.control_sign_up(String.valueOf(first_name), String.valueOf(last_name), String.valueOf(email), String.valueOf(username), String.valueOf(password), String.valueOf(confirm_password), String.valueOf(date_received), String.valueOf(time_received), String.valueOf(sign_up)));
 		out.println("</body>");
 		out.println("</html>");
+		
+		
+      // Recipient's email ID needs to be mentioned.
+      String to = "ltrman1996@hotmail.com";
+
+      // Sender's email ID needs to be mentioned
+      String from = "fromemail@gmail.com";
+      final String username = "manishaspatil";//change accordingly
+      final String password = "******";//change accordingly
+
+      // Assuming you are sending email through relay.jangosmtp.net
+      String host = "relay.jangosmtp.net";
+
+      Properties props = new Properties();
+      props.put("mail.smtp.auth", "true");
+      props.put("mail.smtp.starttls.enable", "true");
+      props.put("mail.smtp.host", host);
+      props.put("mail.smtp.port", "25");
+
+      // Get the Session object.
+      Session session = Session.getInstance(props,
+         new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+               return new PasswordAuthentication(username, password);
+            }
+	});
+
+      try {
+            // Create a default MimeMessage object.
+            Message message = new MimeMessage(session);
+
+   	   // Set From: header field of the header.
+	   message.setFrom(new InternetAddress(from));
+
+	   // Set To: header field of the header.
+	   message.setRecipients(Message.RecipientType.TO,
+              InternetAddress.parse(to));
+
+	   // Set Subject: header field
+	   message.setSubject("Testing Subject");
+
+	   // Send the actual HTML message, as big as you like
+	   message.setContent(
+              "<h1>This is actual message embedded in HTML tags</h1>",
+             "text/html");
+
+	   // Send message
+	   Transport.send(message);
+
+	   out.println("Sent message successfully....");
+
+      } catch (MessagingException e) {
+	   e.printStackTrace();
+	   throw new RuntimeException(e);
+      }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
