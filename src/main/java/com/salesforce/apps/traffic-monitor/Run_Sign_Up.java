db1b5d7416f1ec5356be7ccdc9caef3d14a960c3
Tim van der Graaff <timvdg45@gmail.com>
2019-01-03 21:21:32 -0600

    Update Run_Sign_Up.java

--- src/main/java/com/salesforce/apps/traffic-monitor/Run_Sign_Up.java
+++ src/main/java/com/salesforce/apps/traffic-monitor/Run_Sign_Up.java
@@ -5,6 +5,14 @@ import java.util.Map;
 
 import java.util.Date;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
