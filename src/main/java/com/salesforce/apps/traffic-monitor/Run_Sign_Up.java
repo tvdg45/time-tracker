402c7ffc3bb40bddd2950e6c21e0181bbd5a2715
Tim van der Graaff <timvdg45@gmail.com>
2019-01-03 21:55:53 -0600

    Update Run_Sign_Up.java

--- src/main/java/com/salesforce/apps/traffic-monitor/Run_Sign_Up.java
+++ src/main/java/com/salesforce/apps/traffic-monitor/Run_Sign_Up.java
@@ -5,15 +5,13 @@ import java.util.Map;
 
 import java.util.Date;
 
-import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -24,43 +22,6 @@ import javax.servlet.http.HttpSession;
 
 
 public class Run_Sign_Up extends HttpServlet {
-
-    /*public void sendHtmlEmail(String host, String port,
-            final String userName, final String password, String toAddress,
-            String subject, String message) throws AddressException,
-            MessagingException {
- 
-        // sets SMTP server properties
-        Properties properties = new Properties();
-        properties.put("mail.smtp.host", host);
-        properties.put("mail.smtp.port", port);
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
- 
-        // creates a new session with an authenticator
-        Authenticator auth = new Authenticator() {
-            public PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(userName, password);
-            }
-        };
- 
-        Session session = Session.getInstance(properties, auth);
- 
-        // creates a new e-mail message
-        Message msg = new MimeMessage(session);
- 
-        msg.setFrom(new InternetAddress(userName));
-        InternetAddress[] toAddresses = { new InternetAddress(toAddress) };
-        msg.setRecipients(Message.RecipientType.TO, toAddresses);
-        msg.setSubject(subject);
-        msg.setSentDate(new Date());
-        // set plain text message
-        msg.setContent(message, "text/html");
- 
-        // sends the e-mail
-        Transport.send(msg);
- 
-    }*/
 	
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
@@ -93,6 +54,16 @@ public class Run_Sign_Up extends HttpServlet {
 		out.println("</body>");
 		out.println("</html>");*/
 		
+        String from = "kodejava@gmail.com";
+        String to = "kodejava@gmail.com";
+        String subject = "Hello";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.port", "587");
+        Session session = Session.getDefaultInstance(props);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
