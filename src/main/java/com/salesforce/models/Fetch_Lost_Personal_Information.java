52fa5d71aeb666b2c7888eb27957360248a7df6a
Tim van der Graaff <timvdg45@gmail.com>
2019-01-10 16:46:26 -0600

    Update Fetch_Lost_Personal_Information.java

--- src/main/java/com/salesforce/models/Fetch_Lost_Personal_Information.java
+++ src/main/java/com/salesforce/models/Fetch_Lost_Personal_Information.java
@@ -105,7 +105,7 @@ public abstract class Fetch_Lost_Personal_Information extends Config {
 				}
 			}
 			
-			output = String.valueOf(select_results.getRow()) + "<br />" + get_email + "<br />";
+			output = String.valueOf(select_results.getRow()) + "<br />" + get_email + "<br />" + get_username + "<br />";
 		} catch (Exception e) {
 			
 			LOGGER.log(Level.INFO, "" + e + "");
