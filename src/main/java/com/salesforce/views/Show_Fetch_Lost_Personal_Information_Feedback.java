55b87d93da8b6a4eff44b5fb6fea1690e92a8789
Tim van der Graaff <timvdg45@gmail.com>
2019-01-10 20:31:33 -0600

    Update Show_Fetch_Lost_Personal_Information_Feedback.java

--- src/main/java/com/salesforce/views/Show_Fetch_Lost_Personal_Information_Feedback.java
+++ src/main/java/com/salesforce/views/Show_Fetch_Lost_Personal_Information_Feedback.java
@@ -1,18 +1,18 @@
 //Author: Timothy van der Graaff
 public class Show_Fetch_Lost_Personal_Information_Feedback {
 	
-	public String show_fetch_lost_personal_information_feedback(String[][] fetch_username) {
+	public String show_fetch_lost_personal_information_feedback(String[][] fetch_lost_personal_information) {
 		
 		Config use_config = new Config();
 		
 		String output = "";
 		
-		if (fetch_username[0][0] != "database error") {
+		if (fetch_lost_personal_information[0][0] != "database error") {
 			
-			if (fetch_username[0][0] == "no user action") {
+			if (fetch_lost_personal_information[0][0] == "no user action") {
 				
 				output = "";
-			} else if (fetch_username[0][0] == "successful username authentication") {
+			} else if (fetch_lost_personal_information[0][0] == "successful username authentication") {
 				
 				output += "<script type=\"text/javascript\">\n";
 				output += "$(document).ready(function() {\n\n";
@@ -24,47 +24,11 @@ public class Show_Fetch_Lost_Personal_Information_Feedback {
 				output += "};\n\n";
 				output += "xhttp.open(\"POST\", \"" + use_config.domain() + "/third-party-web-apps/apps/traffic-monitor/fetch-username-email.php\");\n";
 				output += "xhttp.setRequestHeader(\"Content-type\", \"application/x-www-form-urlencoded\");\n";
-				output += "xhttp.send(\"first_name=" + fetch_username[0][1] + "&username=" + fetch_username[0][2] + "&email=" + fetch_username[0][3] + "\");\n";
+				output += "xhttp.send(\"first_name=" + fetch_lost_personal_information[0][1] + "&username=" + fetch_lost_personal_information[0][2] + "&email=" + fetch_lost_personal_information[0][3] + "\");\n";
 				output += "});\n";
 				output += "</script>\n";
 				output += "<div class=\"fetch_username_email\" style=\"text-align: left; width: 100%\"></div>\n\n";
-				output += "<br />" + fetch_username[0][0] + "," + fetch_username[0][1] + "," + fetch_username[0][2] + "," + fetch_username[0][3];
-			} else {
-				
-				output += "<div style=\"text-align: left; width: 100%\">\n";
-				output += "<label>Please correct the following errors:</label>\n";
-				output += "</div>\n";
-				output += "<div style=\"text-align: left; width: 100%\">\n";
-				output += "<ul>\n";
-	
-				for (int row = 0; row < fetch_username.length; row++) {
-
-					if (fetch_username[row][0] != null) {
-						
-						output += "<li><label>" + fetch_username[row][0] + "</label></li>\n";
-					}
-				}
-	
-				output += "</ul>\n";
-				output += "</div>\n";
-			}
-		}
-		
-		return output;
-	}
-	
-	/*public String show_fetch_lost_personal_information_feedback(String[] fetch_password, String search_first_name, String username, String search_email, String search_password) {
-		
-		Config use_config = new Config();
-		
-		String output = "";
-		
-		if (fetch_password[0] != "database error") {
-			
-			if (fetch_password[0] == "no user action") {
-				
-				output = "";
-			} else if (fetch_password[0] == "successful password authentication") {
+			} else if (fetch_lost_personal_information[0][0] == "successful password authentication") {
 				
 				output += "<script type=\"text/javascript\">\n";
 				output += "$(document).ready(function() {\n\n";
@@ -76,7 +40,7 @@ public class Show_Fetch_Lost_Personal_Information_Feedback {
 				output += "};\n\n";
 				output += "xhttp.open(\"POST\", \"" + use_config.domain() + "/third-party-web-apps/apps/traffic-monitor/fetch-password-email.php\");\n";
 				output += "xhttp.setRequestHeader(\"Content-type\", \"application/x-www-form-urlencoded\");\n";
-				output += "xhttp.send(\"first_name=" + search_first_name + "&username=" + username + "&email=" + search_email + "&password=" + search_password + "\");\n";
+				output += "xhttp.send(\"first_name=" + fetch_lost_personal_information[0][1] + "&username=" + fetch_lost_personal_information[0][2] + "&email=" + fetch_lost_personal_information[0][3] + "&password=" + fetch_lost_personal_information[0][4] + "\");\n";
 				output += "});\n";
 				output += "</script>\n";
 				output += "<div class=\"fetch_password_email\" style=\"text-align: left; width: 100%\"></div>\n\n";
@@ -88,19 +52,19 @@ public class Show_Fetch_Lost_Personal_Information_Feedback {
 				output += "<div style=\"text-align: left; width: 100%\">\n";
 				output += "<ul>\n";
 	
-				for (int row = 0; row < fetch_password.length; row++) {
+				for (int row = 0; row < fetch_lost_personal_information.length; row++) {
 
-					if (fetch_password[row] != null) {
+					if (fetch_lost_personal_information[row][0] != null) {
 						
-						output += "<li><label>" + fetch_password[row] + "</label></li>\n";
+						output += "<li><label>" + fetch_lost_personal_information[row][0] + "</label></li>\n";
 					}
 				}
 	
 				output += "</ul>\n";
 				output += "</div>\n";
 			}
-		}		
+		}
 		
 		return output;
-	}*/
+	}
 }
