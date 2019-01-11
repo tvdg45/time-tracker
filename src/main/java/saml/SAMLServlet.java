ce67881a8c0810d5ccdc6705aba461852a719222
Tim van der Graaff <timvdg45@gmail.com>
2019-01-11 11:38:46 -0600

    Update SAMLServlet.java

--- src/main/java/saml/SAMLServlet.java
+++ src/main/java/saml/SAMLServlet.java
@@ -1,7 +1,7 @@
 package saml;
 
-import com.salesforce.util.Bag;
-import com.salesforce.util.XSDDateTime;
+import util.Bag;
+import util.XSDDateTime;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
 import org.apache.commons.httpclient.HttpClient;
