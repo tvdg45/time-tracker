9e7ef21f1c23cee315d94ddafdd12ef9bb0f18e8
Tim van der Graaff <timvdg45@gmail.com>
2019-01-19 08:32:57 -0600

    Update Control_Fetch_Lost_Personal_Information.java

--- src/main/java/controllers/Control_Fetch_Lost_Personal_Information.java
+++ src/main/java/controllers/Control_Fetch_Lost_Personal_Information.java
@@ -7,7 +7,7 @@ public class Control_Fetch_Lost_Personal_Information extends models.Fetch_Lost_P
 	
 	public Control_Fetch_Lost_Personal_Information() {
 		
-		views.Show_Fetch_Lost_Personal_Information_Feedback control_fetch_lost_personal_information = new views.Show_Fetch_Lost_Personal_Information_Feedback();
+		views.Show_Fetch_Lost_Personal_Information_Feedback this.control_fetch_lost_personal_information = new views.Show_Fetch_Lost_Personal_Information_Feedback();
 	}
 	
 	public String control_fetch_username(String email, String fetch_username) {
