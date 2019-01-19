9d66e00548584e595ea77992988b6973709d5418
Tim van der Graaff <timvdg45@gmail.com>
2019-01-19 08:41:09 -0600

    Update Control_Fetch_Lost_Personal_Information.java

--- src/main/java/controllers/Control_Fetch_Lost_Personal_Information.java
+++ src/main/java/controllers/Control_Fetch_Lost_Personal_Information.java
@@ -17,7 +17,7 @@ public class Control_Fetch_Lost_Personal_Information extends models.Fetch_Lost_P
 		this.set_email(email);
 		this.set_fetch_username(fetch_username);
 		
-		output += this.control_fetch_lost_personal_information.show_fetch_lost_personal_information_feedback(this.fetch_username());
+		output += control_fetch_lost_personal_information.show_fetch_lost_personal_information_feedback(this.fetch_username());
 		
 		return output;
 	}
@@ -29,7 +29,7 @@ public class Control_Fetch_Lost_Personal_Information extends models.Fetch_Lost_P
 		this.set_username(username);
 		this.set_fetch_password(fetch_password);
 		
-		output += this.control_fetch_lost_personal_information.show_fetch_lost_personal_information_feedback(this.fetch_password());
+		output += control_fetch_lost_personal_information.show_fetch_lost_personal_information_feedback(this.fetch_password());
 		
 		return output;
 	}
