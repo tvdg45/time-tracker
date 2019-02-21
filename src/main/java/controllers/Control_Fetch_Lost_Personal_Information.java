21d221d54e43bc937e4cf3977917d3ff58f7ee34
Tim van der Graaff <timvdg45@gmail.com>
2019-02-21 00:20:14 -0500

    Update Control_Fetch_Lost_Personal_Information.java

--- src/main/java/controllers/Control_Fetch_Lost_Personal_Information.java
+++ src/main/java/controllers/Control_Fetch_Lost_Personal_Information.java
@@ -3,30 +3,33 @@ package controllers;
 
 public class Control_Fetch_Lost_Personal_Information extends models.Fetch_Lost_Personal_Information {
 	
-	public String control_fetch_username(String email, String fetch_username) {
+	protected views.Show_Fetch_Lost_Personal_Information_Feedback control_fetch_lost_personal_information;
+	
+	public Control_Fetch_Lost_Personal_Information() {
 		
-		views.Show_Fetch_Lost_Personal_Information_Feedback control_fetch_lost_personal_information = new views.Show_Fetch_Lost_Personal_Information_Feedback();
+		control_fetch_lost_personal_information = new views.Show_Fetch_Lost_Personal_Information_Feedback();
+	}
+	
+	public String control_fetch_username(String email, String fetch_username) {
 		
 		String output = "";
 		
 		this.set_email(email);
 		this.set_fetch_username(fetch_username);
 		
-		output += control_fetch_lost_personal_information.show_fetch_lost_personal_information_feedback(this.fetch_username());
+		output += this.control_fetch_lost_personal_information.show_fetch_lost_personal_information_feedback(this.fetch_username());
 		
 		return output;
 	}
 	
 	public String control_fetch_password(String username, String fetch_password) {
 		
-		views.Show_Fetch_Lost_Personal_Information_Feedback control_fetch_lost_personal_information = new views.Show_Fetch_Lost_Personal_Information_Feedback();
-		
 		String output = "";
 		
 		this.set_username(username);
 		this.set_fetch_password(fetch_password);
 		
-		output += control_fetch_lost_personal_information.show_fetch_lost_personal_information_feedback(this.fetch_password());
+		output += this.control_fetch_lost_personal_information.show_fetch_lost_personal_information_feedback(this.fetch_password());
 		
 		return output;
 	}
