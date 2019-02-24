d6016e58eaf4256e8a5233979fd538fc173e2bda
Tim van der Graaff <timvdg45@gmail.com>
2019-02-24 12:04:42 -0500

    Update Show_Change_Third_Party_Account_Feedback.java

--- src/main/java/views/Show_Change_Third_Party_Account_Feedback.java
+++ src/main/java/views/Show_Change_Third_Party_Account_Feedback.java
@@ -1,41 +1,53 @@
 //Author: Timothy van der Graaff
-
 package views;
 
 public class Show_Change_Third_Party_Account_Feedback {
-	
-	public String show_change_third_party_account_feedback(String[] change_third_party_account) {
-		
-		String output = "";
-		
-		if (change_third_party_account[0] == "successful database update" || change_third_party_account[0] == "not logged in") {
-			
-			output += "<script type='text/javascript'>\n";
-			output += "window.location = document.location.href.replace('#', '');\n";
-			output += "</script>\n";
-		} else if (change_third_party_account[0] == "database error" || change_third_party_account[0] == "no user action" || change_third_party_account[0] == "") {
-			
-			output = "";
-		} else {
-			
-			output += "<div style=\"text-align: left; width: 100%\">\n";
-			output += "<label>Please correct the following errors:</label>\n";
-			output += "</div>\n";
-			output += "<div style=\"text-align: left; width: 100%\">\n";
-			output += "<ul>\n";
-	
-			for (int row = 0; row < change_third_party_account.length; row++) {
 
-				if (change_third_party_account[row] != null) {
-					
-					output += "<li><label>" + change_third_party_account[row] + "</label></li>\n";
-				}
-			}
-	
-			output += "</ul>\n";
-			output += "</div>\n";
-		}
-		
-		return output;
-	}
+ public String show_change_third_party_account_feedback(String[] change_third_party_account) {
+
+  String output = "";
+
+  switch (change_third_party_account[0]) {
+
+   case "successful database update":
+   case "not logged in":
+
+    output += "<script type='text/javascript'>\n";
+    output += "window.location = document.location.href.replace('#', '');\n";
+    output += "</script>\n";
+    
+    break;
+
+   case "database error":
+   case "no user action":
+   case "":
+
+    output = "";
+    
+    break;
+
+   default:
+
+    output += "<div style=\"text-align: left; width: 100%\">\n";
+    output += "<label>Please correct the following errors:</label>\n";
+    output += "</div>\n";
+    output += "<div style=\"text-align: left; width: 100%\">\n";
+    output += "<ul>\n";
+
+    for (int row = 0; row < change_third_party_account.length; row++) {
+
+     if (change_third_party_account[row] != null) {
+
+      output += "<li><label>" + change_third_party_account[row] + "</label></li>\n";
+     }
+    }
+
+    output += "</ul>\n";
+    output += "</div>\n";
+    
+    break;
+  }
+
+  return output;
+ }
 }
