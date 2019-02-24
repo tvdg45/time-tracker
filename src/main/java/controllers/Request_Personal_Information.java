0b543e5a80a6dd491755e212dff84655f4a5c5fb
Tim van der Graaff <timvdg45@gmail.com>
2019-02-24 13:09:47 -0500

    Update Request_Personal_Information.java

--- src/main/java/controllers/Request_Personal_Information.java
+++ src/main/java/controllers/Request_Personal_Information.java
@@ -2,20 +2,20 @@
 package controllers;
 
 public class Request_Personal_Information {
-	
-	protected views.Show_Personal_Information request_personal_information;
-	
-	public Request_Personal_Information() {
-		
-		request_personal_information = new views.Show_Personal_Information();
-	}
-	
-	public String admin_request_personal_information(String admin_session) {
-		
-		String output = "";
 
-		output += this.request_personal_information.admin_show_personal_information(admin_session);
-		
-		return output;
-	}
+ protected views.Show_Personal_Information request_personal_information;
+
+ public Request_Personal_Information() {
+
+  request_personal_information = new views.Show_Personal_Information();
+ }
+
+ public String admin_request_personal_information(String admin_session) {
+
+  String output = "";
+
+  output += this.request_personal_information.admin_show_personal_information(admin_session);
+
+  return output;
+ }
 }
