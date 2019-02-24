bbf583d541499bb99f3957fa339e60b5c8bb53dc
Tim van der Graaff <timvdg45@gmail.com>
2019-02-24 13:10:23 -0500

    Update Request_Third_Party_Accounts.java

--- src/main/java/controllers/Request_Third_Party_Accounts.java
+++ src/main/java/controllers/Request_Third_Party_Accounts.java
@@ -2,22 +2,22 @@
 package controllers;
 
 public class Request_Third_Party_Accounts extends models.Third_Party_Accounts_Processor {
-	
-	protected views.Show_Third_Party_Accounts request_third_party_accounts;
-	
-	public Request_Third_Party_Accounts() {
-		
-		request_third_party_accounts = new views.Show_Third_Party_Accounts();
-	}
-	
-	public String request_websites(String admin_session) {
-		
-		String output = "";
-		
-		this.set_admin_session(admin_session);
-		
-		output += this.request_third_party_accounts.show_websites(this.search_websites());
-		
-		return output;
-	}
+
+ protected views.Show_Third_Party_Accounts request_third_party_accounts;
+
+ public Request_Third_Party_Accounts() {
+
+  request_third_party_accounts = new views.Show_Third_Party_Accounts();
+ }
+
+ public String request_websites(String admin_session) {
+
+  String output = "";
+
+  this.set_admin_session(admin_session);
+
+  output += this.request_third_party_accounts.show_websites(this.search_websites());
+
+  return output;
+ }
 }
