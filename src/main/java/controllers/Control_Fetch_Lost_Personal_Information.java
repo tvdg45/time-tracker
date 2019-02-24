2dbe69b32ac481f1633af0975d740483ce803f27
Tim van der Graaff <timvdg45@gmail.com>
2019-02-24 13:08:39 -0500

    Update Control_Fetch_Lost_Personal_Information.java

--- src/main/java/controllers/Control_Fetch_Lost_Personal_Information.java
+++ src/main/java/controllers/Control_Fetch_Lost_Personal_Information.java
@@ -2,35 +2,35 @@
 package controllers;
 
 public class Control_Fetch_Lost_Personal_Information extends models.Fetch_Lost_Personal_Information {
-	
-	protected views.Show_Fetch_Lost_Personal_Information_Feedback control_fetch_lost_personal_information;
-	
-	public Control_Fetch_Lost_Personal_Information() {
-		
-		control_fetch_lost_personal_information = new views.Show_Fetch_Lost_Personal_Information_Feedback();
-	}
-	
-	public String control_fetch_username(String email, String fetch_username) {
-		
-		String output = "";
-		
-		this.set_email(email);
-		this.set_fetch_username(fetch_username);
-		
-		output += this.control_fetch_lost_personal_information.show_fetch_lost_personal_information_feedback(this.fetch_username());
-		
-		return output;
-	}
-	
-	public String control_fetch_password(String username, String fetch_password) {
-		
-		String output = "";
-		
-		this.set_username(username);
-		this.set_fetch_password(fetch_password);
-		
-		output += this.control_fetch_lost_personal_information.show_fetch_lost_personal_information_feedback(this.fetch_password());
-		
-		return output;
-	}
+
+ protected views.Show_Fetch_Lost_Personal_Information_Feedback control_fetch_lost_personal_information;
+
+ public Control_Fetch_Lost_Personal_Information() {
+
+  control_fetch_lost_personal_information = new views.Show_Fetch_Lost_Personal_Information_Feedback();
+ }
+
+ public String control_fetch_username(String email, String fetch_username) {
+
+  String output = "";
+
+  this.set_email(email);
+  this.set_fetch_username(fetch_username);
+
+  output += this.control_fetch_lost_personal_information.show_fetch_lost_personal_information_feedback(this.fetch_username());
+
+  return output;
+ }
+
+ public String control_fetch_password(String username, String fetch_password) {
+
+  String output = "";
+
+  this.set_username(username);
+  this.set_fetch_password(fetch_password);
+
+  output += this.control_fetch_lost_personal_information.show_fetch_lost_personal_information_feedback(this.fetch_password());
+
+  return output;
+ }
 }
