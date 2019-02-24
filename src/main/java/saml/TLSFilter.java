39e515eb72d6b94a5b7be0e7c20f83eeff9052af
Tim van der Graaff <timvdg45@gmail.com>
2019-02-24 13:32:39 -0500

    Update TLSFilter.java

--- src/main/java/saml/TLSFilter.java
+++ src/main/java/saml/TLSFilter.java
@@ -9,35 +9,35 @@ import javax.servlet.http.HttpServletResponse;
 public class TLSFilter implements Filter {
 
 
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
+ @Override
+ public void init(FilterConfig filterConfig) throws ServletException {
 
-    }
+ }
 
-    public void doFilter(ServletRequest request, ServletResponse response,  FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
-        String scheme = httpRequest.getHeader("x-forwarded-proto");
+ public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+  HttpServletRequest httpRequest = (HttpServletRequest) request;
+  HttpServletResponse httpResponse = (HttpServletResponse) response;
+  String scheme = httpRequest.getHeader("x-forwarded-proto");
 
-        if(!scheme.equals("https")) {
-            StringBuilder tlsURL = new StringBuilder("https://");
-            tlsURL.append(request.getServerName());
-            if (httpRequest.getRequestURI() != null) {
-                tlsURL.append(httpRequest.getRequestURI());
-            }
-            if (httpRequest.getQueryString() != null) {
-                tlsURL.append("?").append(httpRequest.getQueryString());
-            }
+  if (!scheme.equals("https")) {
+   StringBuilder tlsURL = new StringBuilder("https://");
+   tlsURL.append(request.getServerName());
+   if (httpRequest.getRequestURI() != null) {
+    tlsURL.append(httpRequest.getRequestURI());
+   }
+   if (httpRequest.getQueryString() != null) {
+    tlsURL.append("?").append(httpRequest.getQueryString());
+   }
 
-            httpResponse.sendRedirect(tlsURL.toString());
+   httpResponse.sendRedirect(tlsURL.toString());
 
-        } else {
-            if (chain != null) chain.doFilter(request, response);
-        }
-    }
+  } else {
+   if (chain != null) chain.doFilter(request, response);
+  }
+ }
 
-    @Override
-    public void destroy() {
+ @Override
+ public void destroy() {
 
-    }
+ }
 }
