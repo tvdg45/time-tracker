d5d10edfc247d4ad2cb060b51f237c70b9e519dd
Tim van der Graaff <timvdg45@gmail.com>
2019-02-24 12:08:47 -0500

    Update Show_Personal_Information_Changer_Feedback.java

--- src/main/java/views/Show_Personal_Information_Changer_Feedback.java
+++ src/main/java/views/Show_Personal_Information_Changer_Feedback.java
@@ -2,42 +2,54 @@
 package views;
 
 public class Show_Personal_Information_Changer_Feedback {
-	
-	public String show_personal_information_changer_feedback(String[] change_personal_information) {
-		
-		String output = "";
-		
-		if (change_personal_information[0] != "database error") {
-			
-			if (change_personal_information[0] == "no user action") {
-				
-				output = "";
-			} else if (change_personal_information[0] == "successful database update" || change_personal_information[0] == "voided changes" || change_personal_information[0] == "not logged in") {
-				
-				output += "<script type='text/javascript'>\n";
-				output += "window.location = document.location.href.replace('#', '');\n";
-				output += "</script>\n";
-			} else {
-				
-				output += "<div style=\"text-align: left; width: 100%\">\n";
-				output += "<label>Please correct the following errors:</label>\n";
-				output += "</div>\n";
-				output += "<div style=\"text-align: left; width: 100%\">\n";
-				output += "<ul>\n";
-	
-				for (int row = 0; row < change_personal_information.length; row++) {
 
-					if (change_personal_information[row] != null) {
-						
-						output += "<li><label>" + change_personal_information[row] + "</label></li>\n";
-					}
-				}
-	
-				output += "</ul>\n";
-				output += "</div>\n";
-			}
-		}
-		
-		return output;
-	}
+ public String show_personal_information_changer_feedback(String[] change_personal_information) {
+
+  String output = "";
+
+  if (!(change_personal_information[0].equals("database error"))) {
+
+   switch (change_personal_information[0]) {
+       
+    case "no user action":
+        
+     output = "";
+     
+     break;
+        
+    case "successful database update":
+    case "voided changes":
+    case "not logged in":
+        
+     output += "<script type='text/javascript'>\n";
+     output += "window.location = document.location.href.replace('#', '');\n";
+     output += "</script>\n";
+     
+     break;
+        
+    default:
+        
+     output += "<div style=\"text-align: left; width: 100%\">\n";
+     output += "<label>Please correct the following errors:</label>\n";
+     output += "</div>\n";
+     output += "<div style=\"text-align: left; width: 100%\">\n";
+     output += "<ul>\n";
+     
+     for (int row = 0; row < change_personal_information.length; row++) {
+
+      if (change_personal_information[row] != null) {
+
+       output += "<li><label>" + change_personal_information[row] + "</label></li>\n";
+      }
+     }
+     
+     output += "</ul>\n";
+     output += "</div>\n";
+     
+     break;
+   }
+  }
+
+  return output;
+ }
 }
