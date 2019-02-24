a329d6843bff580c8a9068c7b24a91654fdd2472
Tim van der Graaff <timvdg45@gmail.com>
2019-02-24 12:44:50 -0500

    Update Show_Sign_Up_Feedback.java

--- src/main/java/views/Show_Sign_Up_Feedback.java
+++ src/main/java/views/Show_Sign_Up_Feedback.java
@@ -16,49 +16,59 @@ public class Show_Sign_Up_Feedback {
 
   if (!(sign_up[0].equals("database error"))) {
 
-   if (!sign_up[0].equals("no user action")) {
+   switch (sign_up[0]) {
+       
+    case "no user action":
+    
+     output = "";
+     
+     break;
+        
+    case "successful database update":
+        
+     output += "<script type=\"text/javascript\">\n";
+     output += "$(document).ready(function() {\n\n";
+     output += "$(\"#new_first_name\").val(\"\");\n";
+     output += "$(\"#new_last_name\").val(\"\");\n";
+     output += "$(\"#new_email\").val(\"\");\n";
+     output += "$(\"#new_username\").val(\"\");\n";
+     output += "$(\"#new_password\").val(\"\");\n";
+     output += "$(\"#confirm_new_password\").val(\"\");\n\n";
+     output += "var xhttp = new XMLHttpRequest();\n\n";
+     output += "xhttp.onreadystatechange = function() {\n\n";
+     output += "if (this.readyState == 4 && this.status == 200) {\n\n";
+     output += "$(\".create_new_account\").html(this.responseText);\n";
+     output += "}\n";
+     output += "};\n\n";
+     output += "xhttp.open(\"POST\", \"" + this.use_config.domain() + "/third-party-web-apps/apps/traffic-monitor/sign-up-email.php\");\n";
+     output += "xhttp.setRequestHeader(\"Content-type\", \"application/x-www-form-urlencoded\");\n";
+     output += "xhttp.send(\"first_name=" + first_name + "&last_name=" + last_name + "&username=" + username + "&password=" + password + "&email=" + email + "\");\n";
+     output += "});\n";
+     output += "</script>\n";
+     output += "<div class=\"create_new_account\" style=\"text-align: left; width: 100%\"></div>\n\n";
+     
+     break;
+        
+    default:
+        
+     output += "<div style=\"text-align: left; width: 100%\">\n";
+     output += "<label>Please correct the following errors:</label>\n";
+     output += "</div>\n";
+     output += "<div style=\"text-align: left; width: 100%\">\n";
+     output += "<ul>\n";
+     
+     for (int row = 0; row < sign_up.length; row++) {
 
-    output = "";
-   } else if (!(sign_up[0].equals("successful database update"))) {
+      if (sign_up[row] != null) {
 
-    output += "<script type=\"text/javascript\">\n";
-    output += "$(document).ready(function() {\n\n";
-    output += "$(\"#new_first_name\").val(\"\");\n";
-    output += "$(\"#new_last_name\").val(\"\");\n";
-    output += "$(\"#new_email\").val(\"\");\n";
-    output += "$(\"#new_username\").val(\"\");\n";
-    output += "$(\"#new_password\").val(\"\");\n";
-    output += "$(\"#confirm_new_password\").val(\"\");\n\n";
-    output += "var xhttp = new XMLHttpRequest();\n\n";
-    output += "xhttp.onreadystatechange = function() {\n\n";
-    output += "if (this.readyState == 4 && this.status == 200) {\n\n";
-    output += "$(\".create_new_account\").html(this.responseText);\n";
-    output += "}\n";
-    output += "};\n\n";
-    output += "xhttp.open(\"POST\", \"" + this.use_config.domain() + "/third-party-web-apps/apps/traffic-monitor/sign-up-email.php\");\n";
-    output += "xhttp.setRequestHeader(\"Content-type\", \"application/x-www-form-urlencoded\");\n";
-    output += "xhttp.send(\"first_name=" + first_name + "&last_name=" + last_name + "&username=" + username + "&password=" + password + "&email=" + email + "\");\n";
-    output += "});\n";
-    output += "</script>\n";
-    output += "<div class=\"create_new_account\" style=\"text-align: left; width: 100%\"></div>\n\n";
-   } else {
-
-    output += "<div style=\"text-align: left; width: 100%\">\n";
-    output += "<label>Please correct the following errors:</label>\n";
-    output += "</div>\n";
-    output += "<div style=\"text-align: left; width: 100%\">\n";
-    output += "<ul>\n";
-
-    for (int row = 0; row < sign_up.length; row++) {
-
-     if (sign_up[row] != null) {
-
-      output += "<li><label>" + sign_up[row] + "</label></li>\n";
+       output += "<li><label>" + sign_up[row] + "</label></li>\n";
+      }
      }
-    }
-
-    output += "</ul>\n";
-    output += "</div>\n";
+     
+     output += "</ul>\n";
+     output += "</div>\n";
+     
+     break;
    }
   }
 
