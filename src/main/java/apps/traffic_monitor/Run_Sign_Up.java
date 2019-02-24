e04aa1a3fb0ed5733b3676e2c6d645046480e5bd
Tim van der Graaff <timvdg45@gmail.com>
2019-02-24 13:52:09 -0500

    Update Run_Sign_Up.java

--- src/main/java/apps/traffic_monitor/Run_Sign_Up.java
+++ src/main/java/apps/traffic_monitor/Run_Sign_Up.java
@@ -3,92 +3,85 @@ package apps.traffic_monitor;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
-import java.util.Map;
 
-import java.util.Date;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 
 public class Run_Sign_Up extends HttpServlet {
-	
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    
-    }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        processRequest(request, response);
-    }
+ protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //processRequest(request, response);
-	    
-		response.addHeader("Access-Control-Allow-Origin", "https://www.timothysdigitalsolutions.com");
-		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-		response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-	    
-		PrintWriter out = response.getWriter();
-		
-		configuration.Config use_config = new configuration.Config();
-		controllers.Control_Sign_Up run_sign_up = new controllers.Control_Sign_Up();
-		
-		String first_name = request.getParameter("first_name");
-		String last_name = request.getParameter("last_name");
-		String email = request.getParameter("email");
-		String username = request.getParameter("username");
-		String password = request.getParameter("password");
-		String confirm_password = request.getParameter("confirm_password");
-		String date_received = request.getParameter("date_received");
-		String time_received = request.getParameter("time_received");
-		String sign_up = request.getParameter("sign_up");
-		
-		out.println("<!DOCTYPE html>");
-		out.println("<html xmlns=\"http://www.w3.org/1999/xhtml\">");
-		out.println("<head>");
-		out.println("<title>Sign up form</title>");
-		out.println("</head>");
-		out.println("<body>");
-		out.println("<script type=\"text/javascript\" src=\"" + use_config.domain() + "/backstretch/js/jquery.min.js\"></script>");
-		out.println("<script type=\"text/javascript\" src=\"" + use_config.domain() + "/backstretch/js/jquery.backstretch.js\"></script>");
-		out.println(run_sign_up.control_sign_up(String.valueOf(first_name), String.valueOf(last_name), String.valueOf(email), String.valueOf(username), String.valueOf(password), String.valueOf(confirm_password), String.valueOf(date_received), String.valueOf(time_received), String.valueOf(sign_up)));
-		out.println("</body>");
-		out.println("</html>");
-    }
+ }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        
-		return "Short description";
-    }// </editor-fold>
+ // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+ /**
+  * Handles the HTTP <code>GET</code> method.
+  *
+  * @param request servlet request
+  * @param response servlet response
+  * @throws ServletException if a servlet-specific error occurs
+  * @throws IOException if an I/O error occurs
+  */
+ @Override
+ protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  processRequest(request, response);
+ }
+
+ /**
+  * Handles the HTTP <code>POST</code> method.
+  *
+  * @param request servlet request
+  * @param response servlet response
+  * @throws ServletException if a servlet-specific error occurs
+  * @throws IOException if an I/O error occurs
+  */
+ @Override
+ protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  //processRequest(request, response);
+
+  response.addHeader("Access-Control-Allow-Origin", "https://www.timothysdigitalsolutions.com");
+  response.setHeader("Access-Control-Allow-Credentials", "true");
+  response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+  response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
+  PrintWriter out = response.getWriter();
+
+  configuration.Config use_config = new configuration.Config();
+  controllers.Control_Sign_Up run_sign_up = new controllers.Control_Sign_Up();
+
+  String first_name = request.getParameter("first_name");
+  String last_name = request.getParameter("last_name");
+  String email = request.getParameter("email");
+  String username = request.getParameter("username");
+  String password = request.getParameter("password");
+  String confirm_password = request.getParameter("confirm_password");
+  String date_received = request.getParameter("date_received");
+  String time_received = request.getParameter("time_received");
+  String sign_up = request.getParameter("sign_up");
+
+  out.println("<!DOCTYPE html>");
+  out.println("<html xmlns=\"http://www.w3.org/1999/xhtml\">");
+  out.println("<head>");
+  out.println("<title>Sign up form</title>");
+  out.println("</head>");
+  out.println("<body>");
+  out.println("<script type=\"text/javascript\" src=\"" + use_config.domain() + "/backstretch/js/jquery.min.js\"></script>");
+  out.println("<script type=\"text/javascript\" src=\"" + use_config.domain() + "/backstretch/js/jquery.backstretch.js\"></script>");
+  out.println(run_sign_up.control_sign_up(String.valueOf(first_name), String.valueOf(last_name), String.valueOf(email), String.valueOf(username), String.valueOf(password), String.valueOf(confirm_password), String.valueOf(date_received), String.valueOf(time_received), String.valueOf(sign_up)));
+  out.println("</body>");
+  out.println("</html>");
+ }
+
+ /**
+  * Returns a short description of the servlet.
+  *
+  * @return a String containing servlet description
+  */
+ @Override
+ public String getServletInfo() {
+
+  return "Short description";
+ } // </editor-fold>
 }
