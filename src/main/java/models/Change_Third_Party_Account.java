6f676af27f60446daeb924d86aa3eed9a7ca3373
Tim van der Graaff <timvdg45@gmail.com>
2019-02-24 16:17:59 -0500

    Update Change_Third_Party_Account.java

--- src/main/java/models/Change_Third_Party_Account.java
+++ src/main/java/models/Change_Third_Party_Account.java
@@ -281,7 +281,7 @@ public abstract class Change_Third_Party_Account extends configuration.Config {
  private double convert_to_bytes_from_gigabytes(String input_value) {
 
   DecimalFormat decimal_format = new DecimalFormat("#,##0.00");
-  
+
   double output;
   double dec_input_value;
   double megabytes;
