9e0358af9b56bac336ab09405562f9a8573b6dc1
Tim van der Graaff <timvdg45@gmail.com>
2019-06-22 11:29:03 -0400

    Add files via upload

---
+++ src/main/java/models/Footer_Content_Processor.java
@@ -0,0 +1,119 @@
+//Author: Timothy van der Graaff
+package models;
+
+import java.util.ArrayList;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public abstract class Footer_Content_Processor extends configuration.Config {
+   
+    private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+    
+    private static void create_new_table() {
+        
+        try {
+            
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            Connection connection = DriverManager.getConnection(database_url(), database_username(),
+                    database_password());
+            
+            PreparedStatement create_statement = connection.prepareStatement(
+                    
+                    "CREATE TABLE company_footer_content (row_id INT NOT NULL, footer_content TEXT NOT NULL, " +
+                            "date_received TEXT NOT NULL, time_received TEXT NOT NULL, PRIMARY KEY (row_id)) " +
+                            "ENGINE = MYISAM;");
+            
+            create_statement.execute();
+        } catch (ClassNotFoundException | SQLException e) {
+            
+            LOGGER.log(Level.INFO, "The 'company_footer_content' " +
+                    "table was not created because it already exists.  " +
+                    "This is not necessarily an error.");
+        }
+    }
+    
+    protected static int search_row_count() {
+        
+        int output;
+        
+        try {
+            
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            Connection connection = DriverManager.getConnection(database_url(), database_username(),
+                    database_password());
+            
+            PreparedStatement prepared_statement = connection.prepareStatement("SELECT row_id FROM " +
+                    "company_footer_content ORDER BY row_id DESC");
+            
+            ResultSet result_set = prepared_statement.executeQuery();
+            
+            result_set.last();
+            
+            if (result_set.getRow() > 0) {
+               
+                output = result_set.getRow();
+            } else {
+                
+                output = 0;
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            
+            LOGGER.log(Level.INFO, "{0}", e);
+            
+            output = 0;
+        }
+        
+        return output;
+    }
+    
+    protected static ArrayList<String> search_footer_content() {
+        
+        ArrayList<String> output = new ArrayList<>();
+        int footer_section_count = 0;
+        
+        try {
+           
+           Class.forName("com.mysql.jdbc.Driver");
+            
+           Connection connection = DriverManager.getConnection(database_url(), database_username(), 
+                   database_password());
+           
+           PreparedStatement select_statement = connection.prepareStatement("SELECT footer_content FROM " +
+                   "company_footer_content ORDER BY row_id ASC");
+           
+           ResultSet select_results = select_statement.executeQuery();
+           
+           while (select_results.next()) {
+               
+               output.add("footer content found");
+               output.add(select_results.getString(1));
+               
+               footer_section_count++;
+           }
+           
+           if (footer_section_count == 0) {
+               
+               output.add("footer content not found");
+           }
+        } catch (ClassNotFoundException | SQLException e) {
+
+            LOGGER.log(Level.INFO, "The 'company_footer_content' " +
+                    "table is corrupt or does not exist");
+            
+            create_new_table();
+            
+            output.add("page error");
+        }
+        
+        return output;
+    }
+}
---
+++ src/main/java/models/Website_Name_Processor.java
@@ -0,0 +1,85 @@
+//Author: Timothy van der Graaff
+package models;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import utilities.Form_Validation;
+
+public abstract class Website_Name_Processor extends configuration.Config {
+    
+    private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+    
+    private static void create_new_table() {
+        
+        try {
+            
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            Connection connection = DriverManager.getConnection(database_url(), database_username(),
+                    database_password());
+            
+            PreparedStatement create_statement = connection.prepareStatement(
+                    
+                    "CREATE TABLE company_website_name (row_id INT NOT NULL, name TEXT NOT NULL, date_received " +
+                            "TEXT NOT NULL, time_received TEXT NOT NULL, PRIMARY KEY (row_id)) ENGINE = MYISAM;");
+            
+            create_statement.execute();
+        } catch (ClassNotFoundException | SQLException e) {
+            
+            LOGGER.log(Level.INFO, "The 'company_website_name' " +
+                    "table was not created because it already exists.  " +
+                    "This is not necessarily an error.");
+        }
+    }
+    
+    protected static String search_website_name() {
+        
+        String output = "";
+        int website_name_count = 0;
+        
+        try {
+           Class.forName("com.mysql.jdbc.Driver");
+           
+           Connection connection = DriverManager.getConnection(database_url(), database_username(), 
+                   database_password());
+           
+           PreparedStatement select_statement = connection.prepareStatement("SELECT name FROM company_website_name " +
+                   "ORDER BY row_id DESC LIMIT 1");
+           
+           ResultSet select_results = select_statement.executeQuery();
+           
+           while (select_results.next()) {
+               
+               output = String.valueOf(select_results.getString(1));
+               
+               website_name_count++;
+           }
+           
+           if (Form_Validation.is_string_null_or_white_space(output)) {
+               
+               output = "Timothy\'s Digital Solutions Framework";
+           }
+           
+           if (website_name_count == 0) {
+               
+               output = "Timothy\'s Digital Solutions Framework";
+           }
+        } catch (ClassNotFoundException | SQLException e) {
+            
+            LOGGER.log(Level.INFO, "The 'company_website_name' " +
+                    "table is corrupt or does not exist");
+            
+            create_new_table();
+            
+            output = "Timothy\'s Digital Solutions Framework";
+        }
+        
+        return output;
+    }
+}
