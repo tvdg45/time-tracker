2785d455ae8712ddf009650f6514f65dac15a6f9
Tim van der Graaff <timvdg45@gmail.com>
2019-08-22 22:04:53 -0500

    Update Config.java

--- src/main/java/configuration/Config.java
+++ src/main/java/configuration/Config.java
@@ -1,6 +1,11 @@
 //Author: Timothy van der Graaff
 package configuration;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -14,13 +19,66 @@ public class Config {
     
     private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
     
-    public static Connection openConnection() {
+    private static String database_server;
+    private static String database_username;
+    private static String database_password;
+    private static String database_port;
+    private static String database_name;
+    private static String database_url;
+    
+    public static void call_database_information() throws IOException {
+        
+        URL url_for_get_request = new URL("https://tds-webhook.herokuapp.com/tds-webhook-company-website");
+        
+        String read_line;
+        
+        HttpURLConnection conection = (HttpURLConnection)url_for_get_request.openConnection();
+        conection.setRequestMethod("POST");
+        
+        int response_code = conection.getResponseCode();
+        
+        if (response_code == HttpURLConnection.HTTP_OK) {
+            
+            StringBuilder response;
+            
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(conection.getInputStream()))) {
+                
+                response = new StringBuilder();
+                
+                while ((read_line = in.readLine()) != null) {
+                    
+                    response.append(read_line);
+                }
+            }
+            
+            String[] credentials = response.toString().split("\\s*,\\s*");
+            
+            if (credentials.length == 5) {
+                
+                try {
+
+                    database_server = credentials[0];
+                    database_username = credentials[1];
+                    database_password = credentials[2];
+                    database_port = credentials[3];
+                    database_name = credentials[4];
+                    
+                    database_url = "jdbc:mysql://" + database_server + ":" + database_port + "/" + database_name;
+                } catch (Exception e) {
+                }
+            }
+        }
+    }
+    
+    public static Connection openConnection() throws IOException {
         
         try {
             
             Class.forName("com.mysql.jdbc.Driver");
             
-            Connection connection = DriverManager.getConnection(database_url(), database_username(), database_password());
+            call_database_information();
+            
+            Connection connection = DriverManager.getConnection(database_url, database_username, database_password);
             
             return connection;
         } catch (ClassNotFoundException | SQLException e) {
@@ -39,78 +97,6 @@ public class Config {
         output.add("http://timothysdigitalsolutions.com");
         output.add("http://www.timothysdigitalsolutions.com");
         output.add("https://timothysdigitalsolutions.com");
-        
-        return output;
-    }
-    
-    public static String database_server() {
-        
-        String output = "";
-        
-        //You can use a name like "localhost", a URL, or an IP address.
-        String database_server = "82.163.176.10";
-        
-        output += database_server;
-        
-        return output;
-    }
-    
-    public static String database_port() {
-        
-        String output = "";
-
-        //This method is used for remote database server connections.  Only number values are allowed.
-        String database_port = "3306";
-        
-        output += database_port;
-        
-        return output;
-    }
-    
-    public static String database_name() {
-        
-        String output = "";
-        
-        //Choose your database by defining the following constant.
-        String database_name = "timothys_digital_solutions_company_website";
-        
-        output += database_name;
-        
-        return output;
-    }
-    
-    public static String database_username() {
-        
-        String output = "";
-
-        //Choose your username by defining the following constant.
-        String database_username = "timothys_tim";
-        
-        output += database_username;
-        
-        return output;
-    }
-    
-    public static String database_password() {
-        
-        String output = "";
-
-        //Choose your password by defining the following constant.
-        String database_password = "ranger12";
-        
-        output += database_password;
-        
-        return output;
-    }
-    
-    public static String database_url() {
-        
-        String output = "";
-        
-        //You can define the following constant as a database connection parameter.  Database username and database password are the two other parameters.  That makes a total of three parameters in your database connection.
-        //Example: Connection connection = DriverManager.getConnection(this.database_url(), this.database_username(), this.database_password());
-        String database_url = "jdbc:mysql://" + database_server() + ":" + database_port() + "/" + database_name();
-        output += database_url;
         
         return output;
     }
